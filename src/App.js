d574b0f225e5e1d6deed626aa5d5c96a8942431f
Jarquevious Nelson <jarquevious@gmail.com>
2021-05-06 15:27:01 -0700

    timers work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import ListTimers from './components/ListTimers';
-import NewTimer from './NewTimer';
-import reducers from './reducers/index';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import React from 'react';
-import './App.css';
-
+import ListTimers from "./components/ListTimers";
+import reducers from "./reducers/index";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { update } from "./actions";
+import NewTimer from "./NewTimer";
+import React from "react";
+import "./App.css";
 
 const store = createStore(reducers);
 
+let lastUpdateTime = Date.now();
+setInterval(() => {
+  const now = Date.now();
+  const deltaTime = now - lastUpdateTime;
+  lastUpdateTime = now;
+  store.dispatch(update(deltaTime));
+}, 50);
+
+
 function App() {
   return (
-
-      <Provider store={store}>
-
-        <div className='App'>
-          <h1>TMRZ</h1>
-          {/* Display the new components */}
-          <NewTimer />
-          <ListTimers />
-        </div>
-        
-      </Provider>
+    <Provider store={store}>
+      <div className="App">
+        <h1>TMRZ</h1>
+        {/* Display the new components */}
+        <NewTimer />
+        <ListTimers />
+      </div>
+    </Provider>
   );
 }
 
