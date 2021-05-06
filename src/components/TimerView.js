a47e86ced1d92a05b8422e06a5e59f69a9026ac9
Jarquevious Nelson <jarquevious@gmail.com>
2021-05-06 15:34:47 -0700

    timers work

--- src/components/TimerView.js
+++ src/components/TimerView.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { toggleTimer } from '../actions'
+import { useDispatch } from 'react-redux';
+import { toggleTimer } from '../actions';
+import { formatTime } from '../utils';
+import React from 'react';
 
 export default function TimerView(props) {
     const { index, timer } = props
@@ -9,11 +10,11 @@ export default function TimerView(props) {
     return (
         <div className="TimerView">
             <h2>{timer.name}</h2>
-            <h1>{timer.time}</h1>
+            <h1>{formatTime(timer.time)}</h1>
             <button 
             onClick={() => dispatch(toggleTimer(index))}
             >
-             {timer.isRunning ? "Stop" : "Start"}"
+             {timer.isRunning ? "Stop" : "Start"}
             </button>
         </div>
     )
